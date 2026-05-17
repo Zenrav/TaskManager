f20d272939934fa710eca76059d165b0dccd4edf
Aarav Raina <aaravraina@Aaravs-MacBook-Air.local>
2026-05-18 00:38:06 +0530

    Implemented session management

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -2,6 +2,8 @@ import User from '../models/user.model.js';
 import jwt from 'jsonwebtoken'
 import { JWT_EXPIRES_IN_ACCESS,JWT_EXPIRES_IN_REFRESH,  JWT_SECRET } from '../config/env.js';
 import { aj } from '../config/arcjet.js';
+import sessionModel from '../models/session.model.js';
+import crypto from 'crypto';
 
 const generateToken = (id , JWT_EXPIRATION_DEADLINE) =>{
     return jwt.sign(
@@ -46,11 +48,18 @@ export const register = async(req, res, next) =>{
         const userResponse = newUser.toObject();
         delete userResponse.password;
 
-        const token = generateToken(newUser._id, JWT_EXPIRES_IN_ACCESS);
+        
         const refreshToken = generateToken(newUser._id, JWT_EXPIRES_IN_REFRESH);
+        const refreshTokenHash = crypto.createHash("sha256").update(refreshToken).digest('hex');
+        const session = await sessionModel.create({
+            user: newUser._id,
+            refreshTokenHash,
+            ip : req.ip,
+            userAgent: req.headers["user-agent"]
+        })
 
-
-
+        const token = generateToken(newUser._id, JWT_EXPIRES_IN_ACCESS);
+ 
         res.cookie("refreshToken", refreshToken, {
             httpOnly: true,
             secure: false,
@@ -111,6 +120,13 @@ export const login = async (req, res, next) => {
 
         const token = generateToken(user._id, JWT_EXPIRES_IN_ACCESS);
         const refreshToken = generateToken(user._id, JWT_EXPIRES_IN_REFRESH);
+        const refreshTokenHash = crypto.createHash("sha256").update(refreshToken).digest("hex");
+        const session = await sessionModel.create({
+            user : user._id,
+            refreshTokenHash,
+            ip : req.ip,
+            userAgent : req.headers["user-agent"]
+        });
 
         res.cookie("refreshToken" , refreshToken, {
             httpOnly: true, 
@@ -132,10 +148,33 @@ export const refreshToken = async (req, res, next)=>{
     try{
 
         const refreshToken = req.cookies.refreshToken;
-        const decoded = jwt.verify(refreshToken, JWT_SECRET)
-        const accessToken = generateToken(decoded._id, JWT_EXPIRES_IN_ACCESS);
 
-        const newRefreshToken = generateToken(decoded._id, JWT_EXPIRES_IN_REFRESH);
+        if(!refreshToken){
+            return res.status(401).json({
+                message : "Refresh token not found"
+            })
+        }
+        const decoded = jwt.verify(refreshToken, JWT_SECRET);
+
+        const refreshTokenHash = crypto.createHash("sha256").update(refreshToken).digest("hex");
+        const session = await sessionModel.findOne({
+            refreshTokenHash,
+            revoked: false
+        });
+
+        if(!session){
+            return res.status(401).json({
+                message : "Invalid refresh token"
+            })
+        }
+        const accessToken = generateToken(decoded.id, JWT_EXPIRES_IN_ACCESS);
+
+        const newRefreshToken = generateToken(decoded.id, JWT_EXPIRES_IN_REFRESH);
+
+        const newRefreshTokenHash = crypto.createHash("sha256").update(newRefreshToken).digest("hex");
+        session.refreshTokenHash = newRefreshTokenHash;
+        await session.save();
+
 
         res.cookie("refreshToken" , newRefreshToken, {
             httpOnly: true,
@@ -143,11 +182,7 @@ export const refreshToken = async (req, res, next)=>{
             sameSite: "strict",
             maxAge: 7 * 24 * 60 * 60 * 1000
         })
-        if(!refreshToken){
-            return res.status(401).json({
-                message : "Refresh token not found"
-            })
-        }
+
         return res.status(200).json({
             message : "Access token refreshed successfully",
             token: accessToken
@@ -157,3 +192,41 @@ export const refreshToken = async (req, res, next)=>{
     }
 }
 
+export const logout = async(req, res, next) =>{
+    try{
+
+        const refreshToken = req.cookies.refreshToken;
+        if(!refreshToken){
+            return res.status(400).json({
+                message : "Refresh Token not found"
+            })
+        }
+
+        const refreshTokenHash = crypto.createHash("sha256").update(refreshToken).digest("hex");
+        const session = await sessionModel.findOne({
+            refreshTokenHash,
+            revoked : false
+        });
+
+        if(!session){
+            return res.status(400).json({
+                message : "Invalid refresh token"
+            })
+        }
+
+        session.revoked = true;
+        await session.save();
+        res.clearCookie("refreshToken",{
+            httpOnly: true,
+            secure: false,
+            sameSite: "strict"
+        });
+
+        res.status(200).json({
+            message : "Logged Out successfully"
+        })
+
+    }catch(err){
+        next(err);
+    }
+}
